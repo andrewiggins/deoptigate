1be7cc250338a15a89405420e04147a49e84cab9
Andre Wiggins <andrewiggins@live.com>
2020-05-06 12:05:43 -0700

    Clean up Monaco prototype a bit

--- monaco-prototype/index.js
+++ monaco-prototype/index.js
@@ -1,6 +1,49 @@
-/** @type {(...args: any[]) => any} */
-const amdRequire = window.require
-amdRequire(['require', 'vs/editor/editor.main'], function (require, monaco) {
+// Check out these source files for help with PeekView:
+// 1. ZoneWidget: https://git.io/JfGne
+// 2. PeekViewWidget: https://git.io/JfGnJ
+// 3. ReferenceWidget: https://git.io/JfGnt
+
+const typeOrder = ['code', 'deopt', 'ics']
+function sortMarkers(markers) {
+  return markers.sort((loc1, loc2) => {
+    if (loc1.line != loc2.line) {
+      return loc1.line - loc2.line
+    } else if (loc1.column != loc2.column) {
+      return loc1.column - loc2.column
+    } else if (loc1.type != loc2.type) {
+      return typeOrder.indexOf(loc1.type) - typeOrder.indexOf(loc2.type)
+    } else {
+      return 0
+    }
+  })
+}
+
+const markers = sortMarkers([
+  { line: 9, column: 27, type: 'code' },
+  { line: 11, column: 20, type: 'code' },
+  { line: 95, column: 22, type: 'code' },
+  { line: 98, column: 33, type: 'deopt' },
+  { line: 98, column: 33, type: 'ics' },
+  { line: 98, column: 39, type: 'ics' },
+  { line: 99, column: 18, type: 'deopt' },
+  { line: 109, column: 17, type: 'deopt' },
+  { line: 109, column: 17, type: 'ics' },
+  { line: 111, column: 21, type: 'ics' },
+  { line: 139, column: 26, type: 'code' },
+  { line: 142, column: 34, type: 'deopt' },
+])
+
+function getIcon(type) {
+  if (type == 'code') {
+    return '▲'
+  } else if (type == 'deopt') {
+    return '▼'
+  } else {
+    return '☎'
+  }
+}
+
+function load(require, monaco, peekView) {
   const html = document.getElementById('code').childNodes[1].textContent
 
   var editor = monaco.editor.create(document.getElementById('container'), {
@@ -17,37 +60,16 @@ amdRequire(['require', 'vs/editor/editor.main'], function (require, monaco) {
     [],
     [
       {
-        range: new monaco.Range(3, 1, 3, 1),
+        range: new monaco.Range(11, 20, 11, 20),
         options: {
-          isWholeLine: true,
-          className: 'myDecorClass',
-          glyphMarginClassName: 'myGlyphMarginClass',
-        },
-      },
-      {
-        range: new monaco.Range(7, 7, 7, 7),
-        options: {
-          className: 'phone-decor',
-          beforeContentClassName: 'phone-decor-before',
-          // inlineClassName: 'phone-decor-inline',
+          className: 'v8-deopt-marker',
+          beforeContentClassName: 'v8-deopt-marker-inline',
+          glyphMarginClassName: 'v8-deopt-marker-margin',
           inlineClassNameAffectsLetterSpacing: true,
         },
       },
     ]
   )
-
-  // Add a zone to make hit testing more interesting
-  var viewZoneId = null
-  editor.changeViewZones(function (changeAccessor) {
-    var domNode = document.createElement('div')
-    domNode.style.background = 'lightgreen'
-    viewZoneId = changeAccessor.addZone({
-      afterLineNumber: 3,
-      heightInLines: 3,
-      domNode: domNode,
-    })
-    console.log(viewZoneId)
-  })
 
   // Add a content widget (scrolls inline with text)
   var contentWidget = {
@@ -59,16 +81,8 @@ amdRequire(['require', 'vs/editor/editor.main'], function (require, monaco) {
     getDomNode: function () {
       if (!this.domNode) {
         this.domNode = document.createElement('div')
-        this.domNode.className = 'ics-content-widget'
+        this.domNode.className = 'v8-deopt-marker-content-widget ics'
         this.domNode.innerHTML = '☎'
-        // this.domNode.innerHTML = '&nbsp;';
-
-        // Copy Editor font styles
-        // this.domNode.style.fontSize = '14px';
-        // this.domNode.style.fontFamily = 'Consolas, "Courier New", monospace';
-        // this.domNode.style.fontFeatureSettings = '"liga" 0, "calt" 0';
-        // this.domNode.lineHeight = '19px';
-        // this.domNode.letterSpacing = '0px';
         editor.applyFontInfo(this.domNode)
       }
       return this.domNode
@@ -76,8 +90,8 @@ amdRequire(['require', 'vs/editor/editor.main'], function (require, monaco) {
     getPosition: function () {
       return {
         position: {
-          lineNumber: 7,
-          column: 7,
+          lineNumber: 11,
+          column: 20,
         },
         preference: [monaco.editor.ContentWidgetPositionPreference.EXACT],
       }
@@ -85,91 +99,43 @@ amdRequire(['require', 'vs/editor/editor.main'], function (require, monaco) {
   }
   editor.addContentWidget(contentWidget)
 
-  // Add an overlay widget
-  // var overlayWidget = {
-  //     // allowEditorOverflow: true,
-  // 	domNode: null,
-  // 	getId: function() {
-  // 		return 'my.overlay.widget';
-  // 	},
-  // 	getDomNode: function() {
-  // 		if (!this.domNode) {
-  // 			this.domNode = document.createElement('div');
-  // 			this.domNode.innerHTML = 'My overlay widget';
-  // 			this.domNode.style.background = 'grey';
-  // 			this.domNode.style.right = '30px';
-  // 			this.domNode.style.top = '50px';
-  // 		}
-  // 		return this.domNode;
-  // 	},
-  // 	getPosition: function() {
-  // 		return null;
-  // 	}
-  // };
-  // editor.addOverlayWidget(overlayWidget);
-
-  var output = document.getElementById('output')
-  function showEvent(str) {
-    while (output.childNodes.length > 6) {
-      output.removeChild(output.firstChild.nextSibling.nextSibling)
+  class Test extends peekView.PeekViewWidget {
+    constructor(editor, options) {
+      super(editor, options)
+      this.create()
     }
-    output.appendChild(document.createTextNode(str))
-    output.appendChild(document.createElement('br'))
+    _fillBody(container) {
+      const title = document.createElement('h1')
+      title.textContent = 'Hello World!'
+      container.appendChild(title)
+      console.log(container)
+    }
   }
 
-  // Check out these source files:
-  // 1. ZoneWidget: https://git.io/JfGne
-  // 2. PeekViewWidget: https://git.io/JfGnJ
-  // 3. ReferenceWidget: https://git.io/JfGnt
-  require(['vs/editor/contrib/peekView/peekView'], (peekView) => {
-    console.log(peekView)
-    class Test extends peekView.PeekViewWidget {
-      constructor() {
-        super(editor, options)
-        this.create()
-      }
-      _fillBody(container) {
-        const title = document.createElement('h1')
-        title.textContent = 'Hello World!'
-        container.appendChild(title)
-        console.log(container)
-      }
-    }
+  const options = {
+    className: 'test-peek-widget',
+    keepEditorSelection: true,
+  }
 
-    const options = {
-      className: 'test-peek-widget',
-      keepEditorSelection: true,
-    }
+  const t = new Test(editor, options)
+  t.show(
+    {
+      startLineNumber: 11,
+      startColumn: 20,
+      endLineNumber: 11,
+      endColumn: 20,
+    },
+    18
+  )
 
-    const t = new Test(editor, options)
-    t.show(
-      {
-        startLineNumber: 7,
-        startColumn: 7,
-        endLineNumber: 7,
-        endColumn: 7,
-      },
-      18
-    )
-
-    setTimeout(() => {
-      console.log('Hiding...')
-      t.hide()
-    }, 10 * 1000)
-  })
-
-  editor.onMouseMove(function (e) {
-    showEvent('mousemove - ' + e.target.toString())
-  })
   editor.onMouseDown(function (e) {
-    showEvent('mousedown - ' + e.target.toString())
-    console.log(e.target)
-    console.log(e.target.toString())
+    console.log('mousedown:', e.target.toString())
   })
-  editor.onContextMenu(function (e) {
-    showEvent('contextmenu - ' + e.target.toString())
-  })
-  editor.onMouseLeave(function (e) {
-    showEvent('mouseleave')
-  })
-})
+}
+
+/** @type {(...args: any[]) => any} */
+const amdRequire = window.require
+const deps = amdRequire(
+  ['require', 'vs/editor/editor.main', 'vs/editor/contrib/peekView/peekView'],
+  load
+)
